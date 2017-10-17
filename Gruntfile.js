9a0971ee1deb4cfe9b35c2b03163315ac13b082d
Conor Kelly <ckelly@ona.io>
2017-10-17 19:19:22 +0300

    Init DB

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,11 +17,23 @@ module.exports = function(grunt) {
         cmd: 'source ./bin/activate && python server.py'
       },
 
+      start_psql: {
+        cmd: 'pg_ctl -D ./nc-psql-server/db -l logfile start'
+      },
+
+      stop_psql: {
+        cmd: 'pg_ctl -D ./nc-psql-server/db -l logfile stop'
+      },
+
       // install tasks
       install_react_app: {
         cwd: './nc-react-app/',
         cmd: 'yarn install'
       },
+
+      init_db: {
+        cmd: 'initdb ./nc-psql-server/db'
+      }
 
       install_hub_server: {
         cmd: [
@@ -49,5 +61,6 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['run']);
 
   grunt.registerTask('run', ['concurrent:dev']);
-  grunt.registerTask('install', ['exec:install_hub_server', 'exec:install_react_app']);
+  grunt.registerTask('kill', ['exec:stop_psql']);
+  grunt.registerTask('install', ['exec:init_db', 'exec:install_hub_server', 'exec:install_react_app']);
 };
